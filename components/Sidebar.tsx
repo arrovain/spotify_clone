62be615d17eb9a4971d0e55445358f2c89e0fa13
arrovain <arrovain@gmail.com>
2024-05-12 19:02:56 +0300

    useMemo paths

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,9 @@
+"use client";
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 import React from "react";
 interface SidebarProps {
   children: React.ReactNode;
@@ -5,6 +11,25 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   return <div>{children}</div>;
+  const pathname = usePathname();
+  const player = usePlayer();
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    []
+  );
 };
 
 export default Sidebar;
