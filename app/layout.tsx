2e7d7dafe7cf1e5a50f452f2f4256e2829948914
arrovain <arrovain@gmail.com>
2024-05-13 15:42:01 +0300

    wrap the sidebar supabase provider and userprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
