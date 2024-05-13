33faad0ce4706a0dac94f30381a0526ae507f5ab
arrovain <arrovain@gmail.com>
2024-05-13 13:49:03 +0300

    routes.map and library codes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,26 @@
 "use client";
-import { useMemo } from "react";
+
 import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import React from "react";
+import { twMerge } from "tailwind-merge";
+
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
+import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
 
 interface SidebarProps {
   children: React.ReactNode;
+  songs: Song[];
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
-  return <div>{children}</div>;
+const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
   const player = usePlayer();
+
   const routes = useMemo(
     () => [
       {
@@ -28,18 +36,30 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         href: "/search",
       },
     ],
-    [pathname]);
-    return (
-      <div className="flex h-full">
-        <div  className='hidden md:Flex'>
-          <Box>
-            
-          </Box>
-         
-        </div>
+    []
+  );
 
+  return (
+    <div
+      className={twMerge(
+        `flex h-full`,
+        player.activeId && "h-[calc(100%-80px)]"
+      )}
+    >
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library songs={songs} />
+        </Box>
       </div>
-    )
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
   );
 };
 
